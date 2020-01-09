2e287aa0b1bc8dc19cf5ed8aa8c8c2281c7de4e3
Oaflias <owl0614@naver.com>
2020-01-09 20:57:53 +0900

    Error Fixed

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-main(){
+int main(){
   printf("Hello World!");
 
   printf("\n choi jun hee");
   printf("\n lee seong won");
-  
-  
   printf("\ntest");
+  return 0;
+}
 
 void love(){
-    printf("I love 우유")
+  printf("I love 우유");
+}
