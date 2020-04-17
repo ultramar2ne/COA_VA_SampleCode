c752b6d6da6fe595b6811fa403cb23172776bedb
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-04-17 23:28:08 +0900

    Update minesweeper.py

--- PyGame/Game/minesweeper.py
+++ PyGame/Game/minesweeper.py
@@ -15,8 +15,9 @@ OPEN_COUNT = 0
 CHECKED = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
 
 pygame.init()
-SURFACE = pygame.display.set_mode([WIDTH*SIZE, HEIGHT*SIZE])
+SURFACE = pygame.display.set_mode([WIDTH * SIZE, HEIGHT * SIZE])
 FPSCLOCK = pygame.time.Clock()
+
 
 def num_of_bomb(field, x_pos, y_pos):
     count = 0
@@ -26,41 +27,43 @@ def num_of_bomb(field, x_pos, y_pos):
             if 0 <= xpos < WIDTH and 0 <= ypos < HEIGHT and field[ypos][xpos] == BOMB:
                 count += 1
     return count
-    
+
+
 def open_tile(field, x_pos, y_pos):
     global OPEN_COUNT
     if CHECKED[y_pos][x_pos]:
         return
-    
+
     CHECKED[y_pos][x_pos] = True
-    
+
     for yoffset in range(-1, 2):
-        for xoffset in range(-1,2):
+        for xoffset in range(-1, 2):
             xpos, ypos = (x_pos + xoffset, y_pos + yoffset)
-            if 0 <= xpos < WIDTH and 0 <= ypos < HEIGHT and  field[ypos][xpos] == EMPTY:
+            if 0 <= xpos < WIDTH and 0 <= ypos < HEIGHT and field[ypos][xpos] == EMPTY:
                 field[ypos][xpos] = OPENED
                 OPEN_COUNT += 1
                 count = num_of_bomb(field, xpos, ypos)
                 if count == 0 and not (xpos == x_pos and ypos == y_pos):
                     open_tile(field, xpos, ypos)
 
+
 def main():
-    smallfront = pygame.font.SysFont(None, 36)
+    smallfont = pygame.font.SysFont(None, 36)
     largefont = pygame.font.SysFont(None, 72)
     message_clear = largefont.render('!!CLEARED!!', True, (0, 255, 255))
     message_over = largefont.render('GAME OVER!!', True, (0, 255, 255))
     message_rect = message_clear.get_rect()
-    message_rect.center = (WIDTH*SIZE/2, HEIGHT*SIZE/2)
+    message_rect.center = (WIDTH * SIZE / 2, HEIGHT * SIZE / 2)
     game_over = False
     field = [[EMPTY for xpos in range(WIDTH)] for ypos in range(HEIGHT)]
-    
+
     count = 0
     while count < NUM_OF_BOMBS:
-        xpos, ypos = randint(0, WIDTH-1), randint(0, HEIGHT-1)
+        xpos, ypos = randint(0, WIDTH - 1), randint(0, HEIGHT - 1)
         if field[ypos][xpos] == EMPTY:
             field[ypos][xpos] = BOMB
             count += 1
-            
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -72,37 +75,38 @@ def main():
                     game_over = True
                 else:
                     open_tile(field, xpos, ypos)
-                    
-    SURFACE.fill((0,0,0))
-    for ypos in range(HEIGHT):
-        for xpos in range(WIDTH):
-            tile = field[ypos][xpos]
-            rect = (xpos*SIZE, ypos*SIZE, SIZE, SIZE)
-            
-            if tile == EMPTY or tile == BOMB:
-                pygame.draw.rect(SURFACE, (192, 192, 192), rect)
-                if game_over and tile == BOMB:
-                    pygame.draw.ellipse(SURFACE, (255, 255, 0), rect)
-            
-            elif tile == OPENED:
-                count = num_of_bomb(field, xpos, ypos)
-                if count > 0:
-                    num_image = smallfont.render('{}'.format(count), True, (255, 255, 0))
-                    SURFACE.blit(num_image, (xpos*SIZE+10, ypos*SIZE+10))
-                    
-    for index in range(0, WIDTH*SIZE, SIZE):
-        pygame.draw.line(SURFACE, (96, 96, 96), (index, 0), (index, HEIGHT*SIZE))
-    
-    for index in range(0, HEIGHT*SIZE, SIZE):
-        pygame.draw.line(SURFACE, (96, 96, 96), (0, index), (WIDTH*SIZE, index))
-        
-    if OPEN_COUNT == WIDTH*HEIGHT - NUM_OF_BOMBS:
-        SURFACE.blit(message_clear, message_rect.topleft)
-    elif game_over:
-        SURFACE.blit(message_over, message_rect.topleft)
-    
-    pygame.display.update()
-    FPSCLOCK.tick(60)
 
-if __name__=='__main__':
+        SURFACE.fill((0, 0, 0))
+        for ypos in range(HEIGHT):
+            for xpos in range(WIDTH):
+                tile = field[ypos][xpos]
+                rect = (xpos * SIZE, ypos * SIZE, SIZE, SIZE)
+
+                if tile == EMPTY or tile == BOMB:
+                    pygame.draw.rect(SURFACE, (192, 192, 192), rect)
+                    if game_over and tile == BOMB:
+                        pygame.draw.ellipse(SURFACE, (255, 255, 0), rect)
+
+                elif tile == OPENED:
+                    count = num_of_bomb(field, xpos, ypos)
+                    if count > 0:
+                        num_image = smallfont.render('{}'.format(count), True, (255, 255, 0))
+                        SURFACE.blit(num_image, (xpos * SIZE + 10, ypos * SIZE + 10))
+
+        for index in range(0, WIDTH * SIZE, SIZE):
+            pygame.draw.line(SURFACE, (96, 96, 96), (index, 0), (index, HEIGHT * SIZE))
+
+        for index in range(0, HEIGHT * SIZE, SIZE):
+            pygame.draw.line(SURFACE, (96, 96, 96), (0, index), (WIDTH * SIZE, index))
+
+        if OPEN_COUNT == WIDTH * HEIGHT - NUM_OF_BOMBS:
+            SURFACE.blit(message_clear, message_rect.topleft)
+        elif game_over:
+            SURFACE.blit(message_over, message_rect.topleft)
+
+        pygame.display.update()
+        FPSCLOCK.tick(60)
+
+
+if __name__ == '__main__':
     main()
