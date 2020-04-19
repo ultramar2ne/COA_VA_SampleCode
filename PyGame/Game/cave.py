50895d364d180299bb0170a0dee4eeb41dc64fbc
SnowyOwl <44254075+SnovvyOwl@users.noreply.github.com>
2020-04-19 23:31:53 +0900

    Update cave.py

--- PyGame/Game/cave.py
+++ PyGame/Game/cave.py
@@ -40,21 +40,21 @@ def main():
             velocity += -3 if is_space_down else 3
             ship_y += velocity
 
-            edge = holes[-1].copy()
-            test = edge.move(0, slope)
+            edge = holes[-1].copy()  # 가장오른쪽에 있는 Rect을 copy함
+            test = edge.move(0, slope)  # SLope 값만큼 이동
             if test.top <= 0 or test.bottom >= 600:
-                slope = randint(1, 6) * (-1 if slope > 0 else 1)
-                edge.inflate_ip(0, -20)
+                slope = randint(1, 6) * (-1 if slope > 0 else 1)  # 부딫혔을때 방향바꿈
+                edge.inflate_ip(0, -20)  # Size를 변경함
             edge.move_ip(10, slope)
-            holes.append(edge)
-            del holes[0]
-            holes =[x.move(-10, 0) for x in holes]
+            holes.append(edge)  # 위치가 변경된 edge를 리스트에 더해줌
+            del holes[0]  # 가장 왼쪽 Rect 삭제
+            holes = [x.move(-10, 0) for x in holes]  # 전체 Holes를 왼쪽을 10만큼 이동
             if holes[0].top > ship_y or holes[0].bottom < ship_y + 40:
                 game_over = True
 
-        SURFACE.fill((0, 255, 0))
+        SURFACE.fill((0, 255, 0))  # 기본 색이 초록이다.
         for hole in holes:
-            pygame.draw.rect(SURFACE, (0, 0, 0), hole)
+            pygame.draw.rect(SURFACE, (0, 0, 0), hole)  # 초록 바탕에다가 검정색 사각형을 그리기
         SURFACE.blit(ship_image, (0, ship_y))
         score_image = sysfont.render("Score is {}".format(score), True, (0, 0, 255))
         SURFACE.blit(score_image, (600, 20))
